6ffab144c3e45dff4829d63419caaef884a97616
Jon Packer <contact@jonpacker.com>
2013-11-15 21:57:11 +0100

    save checkin

--- routes/checkin.js
+++ routes/checkin.js
@@ -2,4 +2,9 @@ module.exports = function(app) {
   app.get('/check_in', function(req, res) {
     res.render('check_in');
   });
+  app.post('/check_in', function(req, res) {
+    app.db.models.CheckIn.save(req.body, function(err, checkIn) {
+      res.send('OK!');
+    });
+  });
 };
