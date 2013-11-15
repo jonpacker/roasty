ed142cd91665d06525e0902444004659cc31fd50
Jon Packer <contact@jonpacker.com>
2013-11-15 22:11:50 +0100

    add timestamps to checkins

--- models/check_in.js
+++ models/check_in.js
@@ -2,4 +2,5 @@ var Model = require('seraph-model');
 module.exports = function(db) {
   var CheckIn = db.models.CheckIn = Model(db, 'check_in');
   CheckIn.fields = ['roastery', 'coffee', 'comment', 'location', 'brew_method'];
+  CheckIn.useTimestamps();
 };
