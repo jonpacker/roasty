643b77f911e6563356fa89145ed689de4852bd73
Jon Packer <contact@jonpacker.com>
2013-11-15 22:39:56 +0100

    added pretty age to checkin model

--- models/check_in.js
+++ models/check_in.js
@@ -1,7 +1,12 @@
 var Model = require('seraph-model');
+var moment = require('moment')
 module.exports = function(db) {
   var CheckIn = db.models.CheckIn = Model(db, 'check_in');
   CheckIn.fields = ['roastery', 'coffee', 'comment', 'location', 'brew_method',
                     'origin', 'user'];
   CheckIn.useTimestamps();
+  CheckIn.addComputedField('pretty_age', function(checkin) {
+    var age = moment.duration(video.created - moment.unix(), 'seconds');
+    return age.humanize(true);
+  });
 };
