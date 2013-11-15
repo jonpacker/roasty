9d89db520ff5cfaa149f123ee350082affe99ed9
Jon Packer <contact@jonpacker.com>
2013-11-15 22:23:14 +0100

    add user and origin fields to check in

--- models/check_in.js
+++ models/check_in.js
@@ -1,6 +1,7 @@
 var Model = require('seraph-model');
 module.exports = function(db) {
   var CheckIn = db.models.CheckIn = Model(db, 'check_in');
-  CheckIn.fields = ['roastery', 'coffee', 'comment', 'location', 'brew_method'];
+  CheckIn.fields = ['roastery', 'coffee', 'comment', 'location', 'brew_method',
+                    'origin', 'user'];
   CheckIn.useTimestamps();
 };
