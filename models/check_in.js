3f3b0fe16405d7a178ab0805f1131f68cd9f1206
Jon Packer <contact@jonpacker.com>
2013-11-15 23:03:16 +0100

    add validation that checkin has at least some value

--- models/check_in.js
+++ models/check_in.js
@@ -5,6 +5,11 @@ module.exports = function(db) {
   CheckIn.fields = ['roastery', 'coffee', 'comment', 'location', 'brew_method',
                     'origin', 'user'];
   CheckIn.useTimestamps();
+  CheckIn.on('validate', function(checkin, callback) {
+    if (!(checkin.roastery || checkin.coffee || checkin.brew_method))
+      callback('You need to specify at least a brew method, roastery or coffee');
+    else callback();
+  });
   CheckIn.addComputedField('pretty_age', function(checkin) {
     var age = moment.duration(checkin.created - moment.unix(), 'seconds');
     return age.humanize(true);
